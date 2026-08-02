d4fb372abd823a490225ccb498660fff4327ff80
Animesh <animeshs1798@gmail.com>
2026-08-02 18:16:49 +0530

    backend init

---
+++ backend/index.ts
@@ -0,0 +1,13 @@
+import express , { Express } from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+app.listen(3000, () => console.log("Server running on port 3000"))
